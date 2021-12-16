8837e5bfbabc8914ce5281c7769b59a2046e6c56
Ulisses Telemaco <ulisses.telemaco@gmail.com>
2021-12-16 10:29:31 -0500

    Fixing bug: task not completed after a timer or a message event close the task

--- src/main/java/org/akip/camunda/CamundaTaskDeleteListener.java
+++ src/main/java/org/akip/camunda/CamundaTaskDeleteListener.java
@@ -1,11 +1,32 @@
 package org.akip.camunda;
 
+import org.akip.domain.TaskInstance;
+import org.akip.domain.enumeration.StatusTaskInstance;
+import org.akip.repository.TaskInstanceRepository;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.Optional;
+
 @Component
 public class CamundaTaskDeleteListener implements TaskListener {
 
-    public void notify(DelegateTask delegateTask) {}
+    private final TaskInstanceRepository taskInstanceRepository;
+
+    public CamundaTaskDeleteListener(TaskInstanceRepository taskInstanceRepository) {
+        this.taskInstanceRepository = taskInstanceRepository;
+    }
+
+    public void notify(DelegateTask delegateTask) {
+        Optional<TaskInstance> optionalTaskInstance = taskInstanceRepository.findByTaskId(delegateTask.getId());
+
+        if (optionalTaskInstance.isPresent()) {
+            TaskInstance taskInstance = optionalTaskInstance.get();
+            taskInstance.setStatus(StatusTaskInstance.COMPLETED);
+            taskInstance.endTime(Instant.now());
+            taskInstanceRepository.save(taskInstance);
+        }
+    }
 }
