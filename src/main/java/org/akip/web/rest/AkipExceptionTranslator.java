0dfcf41b0c68d2d70550010b5b40626da516cecc
JMerlugoF <86815712+JMerlugoF@users.noreply.github.com>
2025-06-19 14:34:03 -0300

    Fixing the order in which the BadRequestErrorException is handled
    Co-authored-by: joao-merlugo <joao.merlugo@owse.com.br>

--- src/main/java/org/akip/web/rest/AkipExceptionTranslator.java
+++ src/main/java/org/akip/web/rest/AkipExceptionTranslator.java
@@ -1,11 +1,13 @@
 package org.akip.web.rest;
 
 import org.akip.exception.BadRequestErrorException;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tech.jhipster.web.rest.errors.ProblemDetailWithCause;
@@ -16,10 +18,11 @@ import tech.jhipster.web.util.HeaderUtil;
  * Controller advice to translate the server side exceptions to client-friendly json structures.
  * The error response follows RFC7807 - Problem Details for HTTP APIs (https://tools.ietf.org/html/rfc7807).
  */
-@ControllerAdvice
+@Order(1)
+@RestControllerAdvice
 public class AkipExceptionTranslator extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(BadRequestErrorException.class)
     public ResponseEntity<Object> handleBadRequestErrorException(BadRequestErrorException ex, NativeWebRequest request) {
         ProblemDetailWithCause pdCause = (ProblemDetailWithCause) ex.getBody();
         return handleExceptionInternal((Exception) ex, pdCause, buildHeaders(ex), HttpStatusCode.valueOf(pdCause.getStatus()), request);
